71d4c184d5761c4c281d3a0adc94dc50d01520c0
Lietekuski <167794035+Lietekuski@users.noreply.github.com>
2024-12-05 00:00:21 +0200

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 // Set up PostgreSQL client connection
 const client = new Client({
-    user: "postgres",
-    database: "postgres",
+    user: "admin",
+    database: "booking_system",
     hostname: "localhost",
-    password: "Secret1234!",
+    password: "admin",
     port: 5432,
 });
 
